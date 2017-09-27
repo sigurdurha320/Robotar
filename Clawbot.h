da9184a4d123a49f4f254dbf81a3c625e6a8a3aa
Sigurður Orri Hjaltason <sigurdurhj320@nemi.tskoli.is>
2017-09-27 08:59:12 +0000

    Update Clawbot.h

--- Clawbot.h
+++ Clawbot.h
@@ -1,5 +1,7 @@
+/*C is short for circumference
+  R is short for radius*/
 #define BASETIME 843
-#define r 5.2
-#define U 32.67256
-#define r1 16.5
-#define U1 103.67256
+#define WheelR 5.2
+#define WheelC 32.67256
+#define RobotR 16.5
+#define RobotC 103.67256
