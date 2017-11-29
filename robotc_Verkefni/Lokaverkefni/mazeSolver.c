030cbf93407b38d2610d2b447522d27a436db17a
Sigurður Orri Hjaltason <sigurdurhj320@nemi.tskoli.is>
2017-11-29 10:15:26 +0000

    just thinking

--- robotc_Verkefni/Lokaverkefni/mazeSolver.c
+++ robotc_Verkefni/Lokaverkefni/mazeSolver.c
@@ -20,35 +20,72 @@ If the robort reaches a point where he cant turn, it will go back
 the way he came and proceed to take paths he has not taken before until he has solved the maze.
 */
 
-task main()
-{
-	int kek=8;
-	char logicGates[16][4]=
+bool logicGates[16][4]=
 	{
-		{'0','0','0','0'},
-		{'0','0','0','1'},
-		{'0','0','1','0'},
-		{'0','0','1','1'},
-		{'0','1','0','0'},
-		{'0','1','0','1'},
-		{'0','1','1','0'},
-		{'0','1','1','1'},
-		{'1','0','0','0'},
-		{'1','0','0','1'},
-		{'1','0','1','0'},
-		{'1','0','1','1'},
-		{'1','1','0','0'},
-		{'1','1','0','1'},
-		{'1','1','1','0'},
-		{'1','1','1','1'}
+		{0,0,0,0},
+		{0,0,0,1},
+		{0,0,1,0},
+		{0,0,1,1},
+		{0,1,0,0},
+		{0,1,0,1},
+		{0,1,1,0},
+		{0,1,1,1},
+		{1,0,0,0},
+		{1,0,0,1},
+		{1,0,1,0},
+		{1,0,1,1},
+		{1,1,0,0},
+		{1,1,0,1},
+		{1,1,1,0},
+		{1,1,1,1}
 	};
-	int maze[8][8];
-	//logicGate[maze[x][y]];
-	//x,y.1011   y-floor(y)*1000
+short maze[8][8];
+
+	//logicGate[maze[x][y]];//how to reference a logic gate
+	//logicGate[maze[x][y]][z];//how to reference a direction in a logic gate
+
+/*how to set a logic gate:
+short meh =0;
+if(values[0]>50)//if it's open "up"
+{
+	meh=8
+}
+if(values[1]>50)//if it's open "right"
+{
+	meh=meh+4;
+}
+if(values[2]>50)//if it's open "down"
+{
+	meh=meh+2;
+}
+if(values[3]>50)//if it's open "left"
+{
+	meh=meh+1;
+}
+maze[x][y]=meh;//(must keep track on x and y
+*/
+
 /*[//0 2 tengjast og 1 3 tenjast
 		[0110, 0011, 0100, 0011],
 		[1110, 1001, 0110, 1001],
 		[1110, 0011, 1110, 0001],
 		[1000, 0011, 1101, 0101]
 	]*/
+//the robot has found his exit once maze[x][y] is out of range
+
+//how to navigate to null value gates
+
+
+//backdrack deadEnd gates
+/*
+	while where he is sum of open gates<2
+	go back(to the open side
+	close the open gate in the logic gate
+/*
+
+
+task main()
+{
+
+
 }
